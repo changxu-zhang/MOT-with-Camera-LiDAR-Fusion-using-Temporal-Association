40a444843ef30c973db16131b7929faa3299c43e
changxu-zhang <137781597+changxu-zhang@users.noreply.github.com>
2024-04-01 20:26:14 +0200

    Add centerpoint tracker

---
+++ track/track_utils.py
@@ -0,0 +1,13 @@
+# copy-paste from CenterPoint https://github.com/tianweiy/CenterPoint/blob/master/tools/nusc_tracking/track_utils.py
+import numpy as np
+
+def greedy_assignment(dist):
+  matched_indices = []
+  if dist.shape[1] == 0:
+    return np.array(matched_indices, np.int32).reshape(-1, 2)
+  for i in range(dist.shape[0]):
+    j = dist[i].argmin()
+    if dist[i][j] < 1e16:
+      dist[:, j] = 1e18
+      matched_indices.append([i, j])
+  return np.array(matched_indices, np.int32).reshape(-1, 2)
